3f95baf0ecd22ebe97657ebdfc1dc65438491459
Selçuk İleri <selcukileri98@gmail.com>
2024-05-28 16:41:35 +0300

    Initial Commit

---
+++ AracTakipApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AracTakipApp
+//
+//  Created by Selçuk İleri on 28.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
